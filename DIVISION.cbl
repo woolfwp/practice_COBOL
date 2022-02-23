086edd0963d78ea2268835a50c31c98d8c4c99f4
sebasPolar <sebasgomez1099@gmail.com>
2022-02-23 14:59:38 -0500

    Comienzo de curso cobol, divisiones

---
+++ DIVISION.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. division_example.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM division_example.
